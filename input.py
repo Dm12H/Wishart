09c3eb50e43058c40990b029809c999984589839
Demidov <m.demidov@itelma.su>
2018-12-05 17:23:50 +0300

    working for speed evaluation, but need fixing logic

--- input.py
+++ input.py
@@ -1,14 +1,16 @@
 import csv
 import numpy as np
 import time
-from wishart import knn
-
+from wishart import WishartWHeight
 with open('/home/demidov/Downloads/ts10e6.csv','rb') as f:
     reader = csv.reader(f, dialect='excel')
     data = np.array([ x for x in reader])
 data = data.reshape([-1,3])
 print(type(data))
+wish = WishartWHeight(data,11,0.01)
 tic = time.time()
-distances , indices = knn(data)
+wish.build_clusters()
 toc = time.time()
-print(toc-tic)
+print('cluster construction took {}'.format(toc-tic))
+clusters = wish.ready_clusters
+
--- wishart.py
+++ wishart.py
@@ -1,7 +1,7 @@
 import numpy
 from sklearn.neighbors import NearestNeighbors
 import scipy.special as sp
-import timeit
+import time
 
 from sklearn.neighbors import NearestNeighbors
 import numpy as np
@@ -12,13 +12,49 @@ status_dict = {
     'completed': -1,
     'in_work': 1,
     'false':0}
+
+def p_density(values,k):
+    volume = np.pi ** (k/2) / sp.gamma(k/2 +1)
+    density = k/(values * volume)
+    return density
+
+def significant(cluster,thresh):
+    max_val = np.max(cluster)
+    min_val = np.min(cluster)
+    res = abs(max_val-min_val)
+    return res >= thresh
+
+def sort_indices(values, neighbors):
+    new_idx = np.argsort(values)
+    idx_dict = {old: new for old, new in zip(range(len(values)), new_idx)}
+    new_neighbors = np.array([[idx_dict[x] for x in y] for y in neighbors])
+    final_sort = np.argsort(new_neighbors[:,0])
+    new_neighbors = new_neighbors[final_sort]
+    new_values = values[new_idx]
+    return new_values, new_neighbors
+
+
 class WishartWHeight(object):
     def __init__(self,feature_list,k,h):
-        uninit_clusters = np.full([len(feature_list),-1])
-        start_status = np.full([len(feature_list),-2])
-        self.feature_list = np.stack([uninit_clusters,start_status])
+        #uninit_clusters = np.full([len(feature_list),-1])
+        #start_status = np.full([len(feature_list),-2])
+        #self.feature_list = np.stack([uninit_clusters,start_status])
         self.k = k
         self.h = h
+        self.open_clusters = {}
+        self.ready_clusters = []
+        self.cur_cls = 1
+        start_time = time.time()
+        radii,indices = self.knn(feature_list)
+        sort_st_time = time.time()
+        print('KNN finished,time elapsed = {:.2f} s'.format(sort_st_time-start_time))
+        radii,indices = sort_indices(radii,indices)
+        print('sorting_finished ,time elapsed = {:.2f} s'.format(time.time() - sort_st_time))
+        self.values = p_density(radii,self.k)
+        self.left_right_split(indices)
+        print('initialization finished. total time elapsed: {:.2f} s').format(time.time()-start_time)
+
+
     def knn(self,x,algorithm = 'kd_tree'):
         """
         :param x: input ndarray of floats of shape [N,d1,d2,d3...dm]
@@ -30,28 +66,101 @@ class WishartWHeight(object):
                  indices - array of indices of shape [N,d1,d2,...,dk,dk+1],
                  where k is number of neighbors.(first coord is just index of element.
         """
-        nbrs = NearestNeighbors(n_neighbors=self.k, algorithm='kd_tree').fit(x)
+        nbrs = NearestNeighbors(n_neighbors=self.k, algorithm=algorithm).fit(x)
         distances, indices = nbrs.kneighbors(x)
         return distances[:,-1], indices
 
-def sort_indices(values,neighbors):
-    new_idx = np.argsort(values)
-    idx_dict = {old:new for old,new in zip(range(len(x)),new_idx)}
-    new_neighbors = np.array([[idx_dict[x] for x in y] for y in neighbors])
-    new_values = values[new_idx]
-    return new_values, new_neighbors
+    def left_right_split(self,indices):
+        assert type(indices) is np.ndarray, \
+        "expected array on input"
+        print(indices.shape)
+        self.vert_clusters = np.full((indices.shape[0], 1), 0)
+        self.status = np.full((indices.shape[0],1),status_dict['undefined'])
+        filler = np.full([1,1],status_dict['filler'])
+        filter_array = np.less(indices[:,1:],np.expand_dims(indices[:,0],1))
+        print(filter_array.shape)
+        status = np.where(filter_array,self.status,filler)
+        self.left_side = np.stack([indices[:,1:],status])
+        print(self.left_side.shape)
 
-def p_density(values,k):
-    volume = np.pi ** (k/2) / sp.gamma(k/2 +1)
-    density = k/(values * volume)
-    return density
+    def build_clusters(self):
+        for idx in xrange(len(self.values)):
+            if idx % 10000 == 0:
+                print(idx)
+            act_verts = np.not_equal(self.left_side[1][idx],2)
+            neighbor_status = self.left_side[0][idx][act_verts]
+            if neighbor_status.size == 0:
+                self.open_clusters[self.cur_cls] = [idx]
+                self.vert_clusters[idx] = self.cur_cls
+                self.status[idx] = status_dict['in_work']
+                self.cur_cls+=1
+                continue
+            classes = self.vert_clusters[neighbor_status]
+            updated_status = self.status[neighbor_status]
+            neighbor_status = np.stack([classes,updated_status])
+            neighbor_clusters = set(neighbor_status[0].flat)
+            if ((len(neighbor_clusters)==1) or
+                (len(neighbor_clusters)==2) and (0 in neighbor_clusters)):
+                if np.any(np.equal(neighbor_status[1],status_dict['in_work'])):
+                    #TODO fix try-except for checking zero in two-element set
+                    try:
+                        neighbor_clusters.remove(0)
+                    except:
+                        pass
+                    target = neighbor_clusters.pop()
+                    self.status[idx] = status_dict['in_work']
+                    self.vert_clusters[idx] = target
+                    self.open_clusters[target].append(idx)
+                    continue
+                else:
+                    self.status[idx] = status_dict['completed']
+                    self.vert_clusters[idx] = 0
+                    continue
+            else:
+                if np.all(np.equal(neighbor_status[1],status_dict['completed'])):
+                    self.status[idx] = status_dict['completed']
+                    self.vert_clusters[idx] = 0
+                elif (len(self.ready_clusters) > 1) or (self.vert_clusters[0] == 0):
+                    self.status[idx] = status_dict['completed']
+                    self.vert_clusters[idx] = 0
+                    # TODO delete normally-work around several repeated neighbors
+                    for x in neighbor_status.T[0]:
+                        if x[1] == status_dict['in_work']:
+                            try:
+                                cluster = np.array(self.open_clusters[x[0]])
+                            except:
+                                continue
+                            if significant(self.values[cluster],self.h):
+                                self.ready_clusters.append(cluster)
+                            del self.open_clusters[x[0]]
+                            self.status[cluster] = status_dict['completed']
+                # need to rewrite this part, left for speed evaluation purposes
+                else:
+                    for x in neighbor_status.T[0]:
+                        self.status[idx] = status_dict['completed']
+                        self.vert_clusters[idx] = 0
+                        if x[1] == status_dict['in_work']:
+                            try:
+                                cluster = np.array(self.open_clusters[x[0]])
+                            except:
+                                continue
+                            cluster = np.array(self.open_clusters[x[0]])
+                            if significant(self.values[cluster],self.h):
+                                self.ready_clusters.append(cluster)
+                            del self.open_clusters[x[0]]
+                            self.status[cluster] = status_dict['completed']
+        for open_cluster in self.open_clusters:
+            cluster = np.array(open_cluster)
+            if significant(self.values[cluster], self.h):
+                self.ready_clusters.append(cluster)
 
-def check_significance(cluster,thresh):
-    max_val = np.max(cluster.values())
-    min_val = np.min(cluster.values())
-    res = np.abs(max_val-min_val)
-    return res >= thresh
 
+
+
+
+
+
+"""
 def neighbors_decision(n_list,num):
     mask = np.not_equal(n_list[1],2)
     act_array = np.extract(np.column_stack((mask, mask)),n_list)
@@ -76,9 +185,6 @@ def reorder_case_1(n_list,cluster_list,thresh):
     return 0, to_remove_list , to_finalize_list
 
 def reorder_case_2(n_list,cluster_list,thresh):
-    
-
-
-#def wishart_graph(density,neighbors):
+"""
 
 
